5e1615d8419f2166aa7cd55032bf7309aa6f4755
Nova <technobaboo@gmail.com>
2021-04-08 14:14:27 -0500

    Fixed crash upon selecting modmenu item with no ID

--- src/main/java/org/technobaboo/resource_extractor/ResourceExtractorMod.java
+++ src/main/java/org/technobaboo/resource_extractor/ResourceExtractorMod.java
@@ -6,16 +6,17 @@ import net.fabricmc.loader.api.ModContainer;
 
 import java.nio.file.Files;
 import java.util.HashMap;
+import java.util.Optional;
 
 public class ResourceExtractorMod implements ClientModInitializer {
 	static protected HashMap<String, Boolean> modsContainAssets = new HashMap<>();
 
-	static private boolean ContainsAssetsInternal(ModContainer mod) {
-		return Files.isDirectory(mod.getPath("assets"));
+	static private boolean ContainsAssetsInternal(Optional<ModContainer> mod) {
+		return mod.isPresent() && Files.isDirectory(mod.get().getPath("assets"));
 	}
 	static public boolean ContainsAssets(String modid) {
 		if(!modsContainAssets.containsKey(modid)) {
-			ModContainer mod = FabricLoader.getInstance().getModContainer(modid).get();
+			Optional<ModContainer> mod = FabricLoader.getInstance().getModContainer(modid);
 			modsContainAssets.put(modid, ContainsAssetsInternal(mod));
 		}
 		return modsContainAssets.get(modid);
@@ -26,7 +27,7 @@ public class ResourceExtractorMod implements ClientModInitializer {
 		System.out.println("Initialized Resource Extractor!");
 
 		for(ModContainer mod : FabricLoader.getInstance().getAllMods()) {
-			modsContainAssets.put(mod.getMetadata().getId(), ContainsAssetsInternal(mod));
+			modsContainAssets.put(mod.getMetadata().getId(), ContainsAssetsInternal(Optional.of(mod)));
 		}
 	}
 }
